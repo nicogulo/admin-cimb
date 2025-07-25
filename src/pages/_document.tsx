dea64db96b54c250b9d158883a8937a080db0bf0
Nico Gulo <nicogulobelas@gmail.com>
2025-07-25 11:57:26 +0700

    refactor: remove unused favicon links from document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,12 +7,6 @@ export default function Document() {
                 {/* Favicon - ICO as primary (standard) */}
                 <link rel="icon" href="/favicon.ico" type="image/x-icon" />
                 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-                <link rel="apple-touch-icon" href="/android-chrome-192x192.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-
-                {/* Additional favicon sizes */}
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 
                 {/* Theme color */}
                 <meta name="theme-color" content="#667eea" />
